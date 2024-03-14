89096d4019b9ad5569f4bf10aab6c65a63d5f3e7
Lee Houk <leehouk@gmail.com>
2024-03-13 20:01:40 -0700

    Configured GetStudio

--- smashgames.client/src/app/data.service.ts
+++ smashgames.client/src/app/data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Studio } from './Models/Studio';
 
 @Injectable({
@@ -10,15 +10,19 @@ export class DataService {
 
   studio$: BehaviorSubject<Studio> = new BehaviorSubject<Studio>({});
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.getStudio(1);
+  }
 
   // hook up our api to return the studio information and save back as the next value of this.studio$
-get studio() {
-    this.http.get<Studio>('http://localhost:3000/studio').subscribe(
-      (data) => {
+
+  //get all studios
+  getAllStudios(): Observable<Studio[]> {
+    return this.http.get<Studio[]>('/api/Studios');  
+  }
+getStudio(id: number) {
+    this.http.get<Studio>('/api/Studios/' + id).subscribe(data => {
         this.studio$.next(data);
-      }
-    );
-    return this.studio$;
+      });
   }
 }
